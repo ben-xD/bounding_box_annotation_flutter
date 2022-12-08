8eb04b73cbb59ca3c7750481e5d33df303fc4f80
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2022-12-08 17:21:18 +0000

    chore: update dummy data

---
+++ backend/schemas/input.sql
@@ -0,0 +1,8 @@
+-- Optional annotation data
+INSERT INTO Annotations (id, AnnotatedOn, ServerReceivedOn, AnnotationJobID, BoundingBoxes) VALUES (
+    "3ed3397f-6a7d-4cb3-a260-f935151dc95b", "20221207 10:00:00", "20221207 10:05:00", "892461f6-b00a-469f-9fd1-42c71bc0fe00", '[{"topLeft":{"dx":10.0,"dy":10.0},"size":{"width":100.0,"height":50.0}}]'
+),(
+    "3ed3397f-6a7d-4cb3-a260-f935151dc95c", "20221207 10:00:00", "20221207 10:05:00", "892461f6-b00a-469f-9fd1-42c71bc0fe00", "[]"
+),(
+    "3ed3397f-6a7d-4cb3-a260-f935151dc95d", "20221207 10:00:00", "20221207 10:05:00", "892461f6-b00a-469f-9fd1-42c71bc0fe00", '[{"topLeft":{"dx":10.0,"dy":10.0},"size":{"width":100.0,"height":50.0}}]'
+);
--- backend/schemas/schema.sql
+++ backend/schemas/schema.sql
@@ -1,5 +1,6 @@
 -- TODO add authors
 
+-- 1. CLEAR EXISTING DATA
 DROP TABLE IF EXISTS Annotations;
 DROP TABLE IF EXISTS AnnotationJobs;
 CREATE TABLE IF NOT EXISTS AnnotationJobs (
@@ -17,17 +18,7 @@ CREATE TABLE IF NOT EXISTS Annotations (
     FOREIGN KEY (AnnotationJobID) REFERENCES AnnotationJobs(id)
 );
 
--- ADD DATA
--- INSERT INTO Annotations (author, body, post_slug) VALUES ("Kristian", "Great post!", "hello-world");
-
+-- 2. Add data for annotation jobs
 INSERT INTO AnnotationJobs (id, CreatedOn, ImageFileName) VALUES ("892461f6-b00a-469f-9fd1-42c71bc0fe00", '20221206 10:00:00', "rodrigo-dos-reis-DkTuGvgPotA-unsplash.jpg");
 INSERT INTO AnnotationJobs (id, CreatedOn, ImageFileName) VALUES ("ce52db24-aa55-45c2-bf1a-4c93b3c0eb61", '20221206 11:00:00', "eiliv-aceron-k9X5yGle-NA-unsplash.jpg");
 INSERT INTO AnnotationJobs (id, CreatedOn, ImageFileName) VALUES ("5be43825-9ae9-457f-82e6-b32fdc7531f7", '20221206 12:00:00', "IMG_4004.jpg");
-
-
-INSERT INTO Annotations (id, AnnotatedOn, ServerReceivedOn, AnnotationJobID, BoundingBoxes) VALUES (
-    "3ed3397f-6a7d-4cb3-a260-f935151dc95b", "20221207 10:00:00", "20221207 10:05:00", "892461f6-b00a-469f-9fd1-42c71bc0fe00", "[]"
-);
-INSERT INTO Annotations (id, AnnotatedOn, ServerReceivedOn, AnnotationJobID, BoundingBoxes) VALUES (
-    "ef74f94d-f1b6-470d-acb2-a456063fa79b", "20221207 10:00:00", "20221207 10:05:00", "892461f6-b00a-469f-9fd1-42c71bc0fe00", "[]"
-);
